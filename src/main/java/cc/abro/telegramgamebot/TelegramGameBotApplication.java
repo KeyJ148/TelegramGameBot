ff899be9e2e4ff16ab1b6451c8cd30a4973284fa
KeyJ148 <kejik148@gmail.com>
2023-02-12 02:21:31 +0400

    Init Spring

---
+++ src/main/java/cc/abro/telegramgamebot/TelegramGameBotApplication.java
@@ -0,0 +1,13 @@
+package cc.abro.telegramgamebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramGameBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramGameBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cc/abro/telegramgamebot/TelegramGameBotApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.abro.telegramgamebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramGameBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
