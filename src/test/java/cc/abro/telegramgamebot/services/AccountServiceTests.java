4bb7f9f8055d764ce7ddf690e1efc38ff598a570
KeyJ148 <kejik148@gmail.com>
2023-03-01 01:15:06 +0400

    Setup settings for tests

---
+++ src/test/java/cc/abro/telegramgamebot/services/AccountServiceTests.java
@@ -0,0 +1,9 @@
+package cc.abro.telegramgamebot.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AccountServiceTests extends DefaultServiceTests {
+
+    @Autowired
+    private AccountService accountService;
+}
