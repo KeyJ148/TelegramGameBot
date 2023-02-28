d67ac81993db6678ed470a04219c575f1859da42
KeyJ148 <kejik148@gmail.com>
2023-03-01 01:16:50 +0400

    Fix id type in repositories

--- src/main/java/cc/abro/telegramgamebot/db/repository/CharacterRepository.java
+++ src/main/java/cc/abro/telegramgamebot/db/repository/CharacterRepository.java
@@ -1,13 +1,12 @@
 package cc.abro.telegramgamebot.db.repository;
 
-import cc.abro.telegramgamebot.db.entity.Account;
+import cc.abro.telegramgamebot.db.entity.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface AccountRepository extends JpaRepository<Account, UUID> {
+public interface CharacterRepository extends JpaRepository<Character, UUID> {
 
-    Account getByTelegramUserId(long telegramUserId);
 }
--- src/main/java/cc/abro/telegramgamebot/db/repository/PlayerRepository.java
+++ src/main/java/cc/abro/telegramgamebot/db/repository/PlayerRepository.java
@@ -1,13 +1,12 @@
 package cc.abro.telegramgamebot.db.repository;
 
-import cc.abro.telegramgamebot.db.entity.Account;
+import cc.abro.telegramgamebot.db.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface AccountRepository extends JpaRepository<Account, UUID> {
+public interface PlayerRepository extends JpaRepository<Player, UUID> {
 
-    Account getByTelegramUserId(long telegramUserId);
 }
