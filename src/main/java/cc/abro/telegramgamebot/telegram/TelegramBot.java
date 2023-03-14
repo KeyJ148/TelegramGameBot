aec2b6b424338bdf1cda02c25bddc0de292609aa
KeyJ148 <kejik148@gmail.com>
2023-03-14 04:48:40 +0400

    Refactoring telegram classes

--- src/main/java/cc/abro/telegramgamebot/telegram/TelegramBot.java
+++ src/main/java/cc/abro/telegramgamebot/telegram/TelegramBot.java
@@ -1,7 +1,34 @@
 package cc.abro.telegramgamebot.telegram;
 
-import org.telegram.telegrambots.meta.generics.LongPollingBot;
+import cc.abro.telegramgamebot.services.TelegramReceiveMessagesService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
-public interface TelegramBot extends LongPollingBot {
-    void init();
+@Component
+@Slf4j
+public class TelegramBot extends TelegramLongPollingBot {
+
+    private final TelegramBotConfig telegramBotConfig;
+    private final TelegramReceiveMessagesService telegramReceiveMessagesService;
+
+    public TelegramBot(DefaultBotOptions defaultBotOptions,
+                       TelegramBotConfig telegramBotConfig,
+                       TelegramReceiveMessagesService telegramReceiveMessagesService) {
+        super(defaultBotOptions, telegramBotConfig.getToken());
+        this.telegramBotConfig = telegramBotConfig;
+        this.telegramReceiveMessagesService = telegramReceiveMessagesService;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        telegramReceiveMessagesService.receiveMessage(null, null); //TODO
+    }
+
+    @Override
+    public String getBotUsername() {
+        return telegramBotConfig.getUsername();
+    }
 }
--- src/main/java/cc/abro/telegramgamebot/telegram/TelegramBotInitializer.java
+++ src/main/java/cc/abro/telegramgamebot/telegram/TelegramBotInitializer.java
@@ -1,16 +1,29 @@
 package cc.abro.telegramgamebot.telegram;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-@Component
+import java.util.List;
+
 @Slf4j
+@Component
+@Profile("prod")
 public class TelegramBotInitializer {
+
+    private final static List<BotCommand> DEFAULT_COMMANDS = List.of(
+            new BotCommand("/start", "Начать игру"),
+            new BotCommand("/help", "Помощь"),
+            new BotCommand("/settings", "Настройки")
+    );
 
     private final TelegramBot telegramBot;
 
@@ -19,13 +32,14 @@ public class TelegramBotInitializer {
     }
 
     @EventListener({ContextRefreshedEvent.class})
-    public void init() {
+    public void init() throws TelegramApiException{
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(telegramBot);
-            telegramBot.init();
+            telegramBot.execute(new SetMyCommands(DEFAULT_COMMANDS, new BotCommandScopeDefault(), null));
         } catch (TelegramApiException e) {
             log.error(e.getMessage());
+            throw e;
         }
     }
 }
