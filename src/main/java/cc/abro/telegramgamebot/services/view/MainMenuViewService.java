2e0f52262f12c95d736bc5d5a60bd506a2835a8e
KeyJ148 <kejik148@gmail.com>
2023-04-10 11:10:29 +0300

    Add buttons messages

--- src/main/java/cc/abro/telegramgamebot/services/view/MainMenuViewService.java
+++ src/main/java/cc/abro/telegramgamebot/services/view/MainMenuViewService.java
@@ -2,40 +2,54 @@ package cc.abro.telegramgamebot.services.view;
 
 import cc.abro.telegramgamebot.db.entity.Account;
 import cc.abro.telegramgamebot.services.CharacterService;
+import cc.abro.telegramgamebot.services.LocalizationService;
 import cc.abro.telegramgamebot.services.TelegramReplyKeyboardService;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 
 import java.util.List;
 
 @Service
 public class MainMenuViewService {
 
+    private final LocalizationService localizationService;
     private final TelegramReplyKeyboardService telegramReplyKeyboardService;
     private final CharacterService characterService;
 
-    public MainMenuViewService(TelegramReplyKeyboardService telegramReplyKeyboardService,
+    public MainMenuViewService(LocalizationService localizationService,
+                               TelegramReplyKeyboardService telegramReplyKeyboardService,
                                CharacterService characterService) {
+        this.localizationService = localizationService;
         this.telegramReplyKeyboardService = telegramReplyKeyboardService;
         this.characterService = characterService;
     }
 
     public ViewResponse getMainMenuView(Account account) {
+        String charactersButton = characterService.getCountCharacters(account.getPlayer()) == 1 ?
+                localizationService.getButton(account, "character") :
+                localizationService.getButton(account, "characters");
+
         List<String> buttons = List.of(
-                characterService.getCountCharacters(account.getPlayer()) == 1 ? "Персонаж" : "Персонажи",
-                "Зоны охоты"
+                charactersButton,
+                localizationService.getButton(account, "zones")
         );
 
-        return new ViewResponse("Вы в главном меню",
-                telegramReplyKeyboardService.createReplyKeyboardVertical(buttons));
+        String textResponse = localizationService.getView(account, "main_menu");
+        ReplyKeyboard keyboardResponse = telegramReplyKeyboardService.createReplyKeyboardVertical(buttons);
+        return new ViewResponse(textResponse, keyboardResponse);
     }
 
     public ViewResponse getMainMenuCharactersView(Account account) {
-        return new ViewResponse("Список персонажей",
-                telegramReplyKeyboardService.createReplyKeyboardHorizontal("Назад"));
+        String textResponse = localizationService.getView(account, "character_list");
+        ReplyKeyboard keyboardResponse = telegramReplyKeyboardService.createReplyKeyboardHorizontal(
+                localizationService.getButton(account, "back"));
+        return new ViewResponse(textResponse, keyboardResponse);
     }
 
     public ViewResponse getMainMenuZonesView(Account account) {
-        return new ViewResponse("Зоны охоты",
-                telegramReplyKeyboardService.createReplyKeyboardHorizontal("Назад"));
+        String textResponse = localizationService.getView(account, "zones");
+        ReplyKeyboard keyboardResponse = telegramReplyKeyboardService.createReplyKeyboardHorizontal(
+                localizationService.getButton(account, "back"));
+        return new ViewResponse(textResponse, keyboardResponse);
     }
 }
