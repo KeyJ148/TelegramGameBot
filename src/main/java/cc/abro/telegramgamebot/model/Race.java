52de19de80718f5c76e823bdfeb05bfafc07eaf7
KeyJ148 <kejik148@gmail.com>
2023-04-17 21:23:20 +0300

    Add races

---
+++ src/main/java/cc/abro/telegramgamebot/model/Race.java
@@ -0,0 +1,7 @@
+package cc.abro.telegramgamebot.model;
+
+public enum Race {
+    HUMAN, ELF, HALF_ELF, ORC, FAIRY,
+    TRITON, MINOTAUR, DWARF, AASIMAR,
+    KENKU, KHAJIIT, WARFORGET, LIZARDFOLK
+}
