f39b535bafca12b3c9dd4fc6464934c165f12c6b
KeyJ148 <kejik148@gmail.com>
2023-04-18 21:24:53 +0300

    Add name validation

--- src/main/java/cc/abro/telegramgamebot/services/gamestates/NewPlayerStateService.java
+++ src/main/java/cc/abro/telegramgamebot/services/gamestates/NewPlayerStateService.java
@@ -4,6 +4,8 @@ import cc.abro.telegramgamebot.db.entity.Account;
 import cc.abro.telegramgamebot.services.CharacterService;
 import cc.abro.telegramgamebot.services.LocalizationService;
 import cc.abro.telegramgamebot.services.view.MainMenuViewService;
+import cc.abro.telegramgamebot.services.view.StartViewService;
+import cc.abro.telegramgamebot.telegram.TelegramBotInitializer;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,13 +14,16 @@ public class NewPlayerStateService implements GameStateService {
     private final CharacterService characterService;
     private final MainMenuViewService mainMenuViewService;
     private final LocalizationService localizationService;
+    private final StartViewService startViewService;
 
     public NewPlayerStateService(CharacterService characterService,
                                  MainMenuViewService mainMenuViewService,
-                                 LocalizationService localizationService) {
+                                 LocalizationService localizationService,
+                                 StartViewService startViewService) {
         this.characterService = characterService;
         this.mainMenuViewService = mainMenuViewService;
         this.localizationService = localizationService;
+        this.startViewService = startViewService;
     }
 
     @Override
@@ -31,9 +36,17 @@ public class NewPlayerStateService implements GameStateService {
             return new GameStateResponse(GameState.NEW_PLAYER,
                     localizationService.getText(account, "new_player.error.too_long"));
         }
+        if (nickname.equals(TelegramBotInitializer.COMMAND_START)) {
+            return new GameStateResponse(GameState.NEW_PLAYER,
+                    startViewService.getStartView(account));
+        }
         if (nickname.contains("/")) {
             return new GameStateResponse(GameState.NEW_PLAYER,
                     localizationService.getText(account, "new_player.error.slash"));
+        }
+        if (!nickname.matches("^[a-zA-Z0-9.]+$")) {
+            return new GameStateResponse(GameState.NEW_PLAYER,
+                    localizationService.getText(account, "new_player.error.latin_only"));
         }
 
         characterService.createMainCharacter(account.getPlayer(), nickname);
