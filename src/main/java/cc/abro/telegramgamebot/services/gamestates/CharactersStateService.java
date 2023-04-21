5f8d1cc5dad0659e47236f70d3c46203f6bdf296
KeyJ148 <kejik148@gmail.com>
2023-04-21 22:17:11 +0300

    Add character state and service

---
+++ src/main/java/cc/abro/telegramgamebot/services/gamestates/CharactersStateService.java
@@ -0,0 +1,40 @@
+package cc.abro.telegramgamebot.services.gamestates;
+
+import cc.abro.telegramgamebot.db.entity.Account;
+import cc.abro.telegramgamebot.services.CharacterService;
+import cc.abro.telegramgamebot.services.LocalizationService;
+import cc.abro.telegramgamebot.services.view.CharacterViewService;
+import cc.abro.telegramgamebot.services.view.MainMenuViewService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CharactersStateService implements GameStateService {
+
+    private final MainMenuViewService mainMenuViewService;
+    private final LocalizationService localizationService;
+    private final CharacterViewService characterViewService;
+    private final CharacterService characterService;
+
+    public CharactersStateService(MainMenuViewService mainMenuViewService,
+                                  LocalizationService localizationService,
+                                  CharacterViewService characterViewService,
+                                  CharacterService characterService) {
+        this.mainMenuViewService = mainMenuViewService;
+        this.localizationService = localizationService;
+        this.characterViewService = characterViewService;
+        this.characterService = characterService;
+    }
+
+    @Override
+    public GameStateResponse processMessage(Account account, String message) {
+        if (message.equals(localizationService.getButton(account, "back"))) {
+            return new GameStateResponse(GameState.MAIN_MENU, mainMenuViewService.getMainMenuView(account));
+        }
+        return null;
+    }
+
+    @Override
+    public GameState getProcessedGameState() {
+        return GameState.CHARACTER;
+    }
+}
--- src/main/java/cc/abro/telegramgamebot/services/gamestates/GameState.java
+++ src/main/java/cc/abro/telegramgamebot/services/gamestates/GameState.java
@@ -2,6 +2,6 @@ package cc.abro.telegramgamebot.services.gamestates;
 
 public enum GameState {
     START, NEW_PLAYER,
-    MAIN_MENU, MAIN_MENU_CHARACTERS, MAIN_MENU_ZONES
-
+    MAIN_MENU, MAIN_MENU_CHARACTERS, MAIN_MENU_ZONES,
+    CHARACTER
 }
